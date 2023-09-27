858ac842b3c923a29a23bb1021285df23374574c
Sam_Guercho <57171996+SamGuercho@users.noreply.github.com>
2023-09-27 06:36:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,50 +1,49 @@
 //Create Web Server
-const express = require('express');
-const app = express();
-const port = 3000;
-//Create a route
-app.get('/', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route
-app.get('/users', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route
-app.get('/comments', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var port = 8080;
+var mongoose = require('mongoose');
+var Comment = require('./models/comment');
+var config = require('./config');
+var router = express.Router();
 
-// Path: users.js
-//Create Web Server
-const express = require('express');
-const app = express();
-const port = 3000;
-//Create a route
-app.get('/', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route
-app.get('/users', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route
-app.get('/comments', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Connect to MongoDB
+mongoose.connect(config.database);
 
-// Path: index.js
-//Create Web Server
-const express = require('express');
-const app = express();
-const port = 3000;
-//Create a route
-app.get('/', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route
-app.get('/users', (req, res) => res.send('Hello World!'));
-//Listen on port 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route
-app.get('/comments', (req, res) => res.send
+//Configure app to use bodyParser()
+//This will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Set static file location for frontend
+app.use(express.static(__dirname + '/public'));
+
+//Routes for API
+router.use(function(req, res, next) {
+  console.log('Something is happening.');
+  next();
+});
+
+//Test route to make sure everything is working
+router.get('/', function(req, res) {
+  res.json({message: 'hooray! welcome to our api!'});
+});
+
+//More routes for API will happen here
+
+//Register routes
+//All routes will be prefixed with /api
+app.use('/api', router);
+
+//Start the server
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+// Path: models/comment.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Comment schema
+var CommentSchema = new Schema({
