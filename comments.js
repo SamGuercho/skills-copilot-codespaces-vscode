299437261e014558a0955b817a30480706aee367
Sam_Guercho <57171996+SamGuercho@users.noreply.github.com>
2023-09-27 06:32:56 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,50 @@
+//Create Web Server
+const express = require('express');
+const app = express();
+const port = 3000;
+//Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route
+app.get('/users', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route
+app.get('/comments', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: users.js
+//Create Web Server
+const express = require('express');
+const app = express();
+const port = 3000;
+//Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route
+app.get('/users', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route
+app.get('/comments', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: index.js
+//Create Web Server
+const express = require('express');
+const app = express();
+const port = 3000;
+//Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route
+app.get('/users', (req, res) => res.send('Hello World!'));
+//Listen on port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route
+app.get('/comments', (req, res) => res.send
